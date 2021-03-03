8441a6dabcba18a32652327a9f04d1ec76cfd92d
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-03 15:32:55 +0100

    testing, implementation and refactoring of add and size functions

--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
 
+    private static final int EMPTY = 0;
+    private static final int ONE_ELEMENT = 1;
+    private static final int START_ITERATIONS = 1;
+    private static final int ITERATIONS = 100;
     CircularList circularList;
 
     @BeforeEach
@@ -17,4 +23,20 @@ public class CircularListTest {
         this.circularList = new CircularListImpl();
     }
 
+    @Test
+    void testAdd() {
+        assertEquals(EMPTY, circularList.size());
+        circularList.add(ONE_ELEMENT);
+        assertEquals(ONE_ELEMENT, circularList.size());
+    }
+    
+    @Test
+    void testSize() {
+        for(int i=START_ITERATIONS; i <=ITERATIONS; i++) {
+            circularList.add(ONE_ELEMENT);
+        }
+        assertEquals(ITERATIONS, circularList.size());
+        
+    }
+    
 }
