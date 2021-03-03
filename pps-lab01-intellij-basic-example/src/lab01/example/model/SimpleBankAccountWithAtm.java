e6c1d16c7c6f2037441fd02fb4829b8ff39abddc
Simone Romagnoli <simone.romagnoli10@studio.unibo.it>
2021-03-03 15:08:39 +0100

    functioning AtmTest refactored using protected fields in SimpleBankAccountTest

--- pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
+++ pps-lab01-intellij-basic-example/src/lab01/example/model/SimpleBankAccountWithAtm.java
@@ -1,18 +1,21 @@
 package lab01.example.model;
 
 public class SimpleBankAccountWithAtm extends SimpleBankAccount implements BankAccount {
+
+    public static final double ATM_FEE = 1;
+
     public SimpleBankAccountWithAtm(AccountHolder holder, double balance) {
         super(holder, balance);
     }
 
     @Override
     public void deposit(final int usrID, final double amount) {
-        super.deposit(usrID, amount-1);
+        super.deposit(usrID, amount-ATM_FEE);
     }
 
     @Override
     public void withdraw(final int usrID, final double amount) {
-        super.withdraw(usrID, amount+1);
+        super.withdraw(usrID, amount+ATM_FEE);
     }
 
 }
--- pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
+++ pps-lab01-intellij-basic-example/test/SimpleBankAccountWithAtmTest.java
@@ -6,36 +6,38 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SimpleBankAccountWithAtmTest extends SimpleBankAccountTest{
 
+    private static final double ATM_FEE = SimpleBankAccountWithAtm.ATM_FEE;
+
     @BeforeEach
     void setBankAccount() {
-        bankAccount = new SimpleBankAccountWithAtm(accountHolder, 0);
+        bankAccount = new SimpleBankAccountWithAtm(accountHolder, EMPTY);
     }
 
     @Test
     void testDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), BIG_AMOUNT);
+        assertEquals(BIG_AMOUNT-ATM_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWrongDeposit() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.deposit(2, 50);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), BIG_AMOUNT);
+        bankAccount.deposit(WRONG_ID, BIG_AMOUNT);
+        assertEquals(BIG_AMOUNT-ATM_FEE, bankAccount.getBalance());
     }
 
     @Test
     void testWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(accountHolder.getId(), 70);
-        assertEquals(30, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), BIG_AMOUNT);
+        bankAccount.withdraw(accountHolder.getId(), MEDIUM_AMOUNT);
+        assertEquals(LITTLE_AMOUNT-ATM_FEE*2, bankAccount.getBalance());
     }
 
     @Test
     void testWrongWithdraw() {
-        bankAccount.deposit(accountHolder.getId(), 100);
-        bankAccount.withdraw(2, 70);
-        assertEquals(100, bankAccount.getBalance());
+        bankAccount.deposit(accountHolder.getId(), BIG_AMOUNT);
+        bankAccount.withdraw(WRONG_ID, MEDIUM_AMOUNT);
+        assertEquals(BIG_AMOUNT-ATM_FEE, bankAccount.getBalance());
     }
 
 }
